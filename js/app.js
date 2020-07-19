c4cf4a3e8277cf75c73c16ebe607fcef75b28fab
waynePovey <wayne.povey@gmail.com>
2020-07-20 00:05:48 +0100

    Added spaceship, asteroids and improved skybox

--- js/app.js
+++ js/app.js
@@ -3,61 +3,54 @@ let scene;
 let camera;
 let renderer;
 const fov = 40;
-let aspect;
 const near = 0.1;
 const far = 20000000;
-let gui;
-let cube0;
-let cube1;
-let controls;
-let p38;
+let flyingSaucer;
 
-function init() {
+async function init() {
   container = document.querySelector('#container');
   
   scene = new THREE.Scene();
-
-  let textureLoader = new THREE.TextureLoader();
-  const ft = textureLoader.load("../assets/textures/purple_space_skybox/purple_space_front5.png", 16);
-  const bk = textureLoader.load("../assets/textures/purple_space_skybox/purple_space_back6.png", 16);
-  const up = textureLoader.load("../assets/textures/purple_space_skybox/purple_space_top3.png", 16);
-  const dn = textureLoader.load("../assets/textures/purple_space_skybox/purple_space_bottom4.png", 16);
-  const rt = textureLoader.load("../assets/textures/purple_space_skybox/purple_space_right1.png", 16);
-  const lf = textureLoader.load("../assets/textures/purple_space_skybox/purple_space_left2.png", 16);
   
-  let materialArray = [
-    new THREE.MeshBasicMaterial({map: rt, side: THREE.BackSide}),
-    new THREE.MeshBasicMaterial({map: lf, side: THREE.BackSide}),
-    new THREE.MeshBasicMaterial({map: up, side: THREE.BackSide}),
-    new THREE.MeshBasicMaterial({map: dn, side: THREE.BackSide}),
-    new THREE.MeshBasicMaterial({map: ft, side: THREE.BackSide}),
-    new THREE.MeshBasicMaterial({map: bk, side: THREE.BackSide}),
-  ];
+  skybox = createSkybox('red_complex', 16, 1000000);
+  skybox.position.set(0, 0, 0);
+  scene.add(skybox);
+  
+  loadModel('../assets/models/flying_saucer/flying_saucer.glb', 'flyingSaucer', 0, 0, 0, 1);
+  
+  loadModel('../assets/models/asteroid/asteroid.glb', 'asteroid01', 20,   -25,   20, 2);
+  loadModel('../assets/models/asteroid/asteroid.glb', 'asteroid02', -20,    0,   30, 5);
+  loadModel('../assets/models/asteroid/asteroid.glb', 'asteroid03', -25,   -5,  -30, 1);
+  loadModel('../assets/models/asteroid/asteroid.glb', 'asteroid04', 0,     10,  -40, 1);
+  loadModel('../assets/models/asteroid/asteroid.glb', 'asteroid05', 60,   -10, -50, 3);
+  loadModel('../assets/models/asteroid/asteroid.glb', 'asteroid06', 20,     5, -60, 6);
+  loadModel('../assets/models/asteroid/asteroid.glb', 'asteroid07', -40,    5,  10, 2);
+  loadModel('../assets/models/asteroid/asteroid.glb', 'asteroid08', -100, -17,  0,  9);
 
-  skybox = new createMesh(
-    new THREE.BoxBufferGeometry(10000000, 10000000, 10000000),
-    materialArray,
-    5, 1, 0
-  );
 
-  loadModel(
-    '../assets/models/p38Lightning/completeP38.glb'  
-  );
   
   createGui();
   createCamera(fov, container.clientWidth, container.clientHeight, near, far, 0, 20, 50);
   createControls();
-  createLights(5, 5, 5, 3);
+  createLighting();
   createRenderer(true, window.devicePixelRatio);
-    
+  
+  console.log(scene);
+
   renderer.setAnimationLoop(() => {
     update();
     render();
   });
+  
 }
 
 function update() {
-  
+  // skybox.rotation.y += 0.0001;
+  // if (scene.getObjectByName('flyingSaucer', true)) {
+  //   const flyingSaucer = scene.getObjectByName('flyingSaucer', true);
+  //   flyingSaucer.rotation.x += 0.0002;
+  //   flyingSaucer.rotation.y += 0.0002;
+  // }
 }
 
 function render() {
@@ -77,20 +70,38 @@ function stop() {
 /* CREATOR FUNCTIONS */
 /*********************/
 
+function createSkybox(textureName, aa, size) {
+  const textureLoader = new THREE.TextureLoader();
+  
+  const sides = ['back', 'front', 'top', 'bottom', 'left', 'right'];
+  const materialArray = sides.map(side => {
+    return new THREE.MeshBasicMaterial({
+      map: textureLoader.load('../assets/textures/' + textureName + '/' + textureName + '_' + side + '.png', aa),
+      side: THREE.BackSide });
+  });
+  
+  return new THREE.Mesh(
+    new THREE.BoxBufferGeometry(size, size, size),
+    materialArray,
+  );
+}
+
 function createCamera(fov, width, height, near, far, x, y, z) {
   camera = new THREE.PerspectiveCamera(fov, width / height, near, far);
   camera.position.set(x, y, z);
 }
 
-function createLights(x, y, z, intensity) {
-  const dLight = new THREE.DirectionalLight(0xffffff, intensity);
-  dLight.position.set(x, y, z);
-  // const aLight = new THREE.HemisphereLight(0xddeeff, 0x202020, intensity);
+function createLighting() {
+  const star = new THREE.DirectionalLight(0xfcff8a, 2);
+  star.position.set(-300, 0, 0);
+  scene.add(star);
 
-  const aLight = new THREE.AmbientLight(0xffffff, intensity);
-  
+  const backlight = new THREE.DirectionalLight(0xed81c2, 1);
+  backlight.position.set(300000, 0, 0);
+  scene.add(backlight);
+
+  const aLight = new THREE.AmbientLight(0xffffff, 1);
   scene.add(aLight);
-  scene.add(dLight);
 }
 
 function createRenderer(aa, ratio) {
@@ -122,24 +133,17 @@ function loadTexture(texUrl, AF) {
   return texture;
 }
 
-function createMesh(geometry, material, x, y, z) {
-  const mesh = new THREE.Mesh(geometry, material);
-  
-  mesh.position.x = x;
-  mesh.position.y = y;
-  mesh.position.z = z;
-  
-  scene.add(mesh);
-  
-  return mesh;
-}
-
-function loadModel(modelUrl, vector3) {
+function loadModel(modelUrl, name, x, y, z, scale) {
   const loader = new THREE.GLTFLoader();
   loader.load(modelUrl, (gltf) => {
-    console.log(gltf);
-
-    p38 = gltf.scene.children;
+    
+    gltf.scene.position.x = x;
+    gltf.scene.position.y = y;
+    gltf.scene.position.z = z;
+    gltf.scene.scale.x = scale;
+    gltf.scene.scale.y = scale;
+    gltf.scene.scale.z = scale;
+    gltf.scene.name = name;
 
     scene.add(gltf.scene);
   });
